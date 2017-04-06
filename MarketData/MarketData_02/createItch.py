a5e56a19e7a3855fdf6084bb6accbb211ceb98e6
John Stratoudakis <johnstratoudakis@gmail.com>
2017-04-06 18:17:05 -0400

    Refactoring create itch script

--- MarketData/MarketData_02/createItch.py
+++ MarketData/MarketData_02/createItch.py
@@ -2,31 +2,41 @@
 
 from Itch41 import *
 
-# Begin code for generating a bunch of messages
-messages = [ ]
-messages.append( [          MessageType.TimeStamp, {     Field.Seconds: 100 } ] )
-messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   10, Field.OrderRefNum: 1, Field.Side: 'B', Field.Shares: 200, Field.Stock: "AAPL", Field.Price: 100.53 } ] )
-messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   20, Field.OrderRefNum: 2, Field.Side: 'B', Field.Shares: 300, Field.Stock: "AAPL", Field.Price: 100.55 } ] )
-messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   30, Field.OrderRefNum: 3, Field.Side: 'B', Field.Shares: 400, Field.Stock: "AAPL", Field.Price: 100.56 } ] )
-messages.append( [      MessageType.OrderExecuted, { Field.NanoSeconds:   40, Field.OrderRefNum: 2, Field.Shares: 300, Field.MatchNum: 1001 } ] )
+
+def generateMessages(messages):
+    genMessages = [ ]
+    for message in messages:
+        msg = ItchMessageFactory.createFromArgs(message)
+        if msg:
+            genMessages.append(msg)
+    return genMessages
+
+def saveMessagesToFile(messages, fileName):
+    print("Creating file: {}".format(fileName))
+    import os
+    os.remove(fileName)
+    for message in genMessages:
+        print("-- Dumping message #{0} --".format(i))
+        message.dumpPretty()
+        message.appendToFile(outFile)
+
+def create_Test_1():
+    # Create 3 Add Order messages
+    # Execute Order #2
+    outFile = "Itch.test1.dat"
+
+    # Begin code for generating a bunch of messages
+    messages = [ ]
+    messages.append( [          MessageType.TimeStamp, {     Field.Seconds: 100 } ] )
+    messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   10, Field.OrderRefNum: 1, Field.Side: 'B', Field.Shares: 200, Field.Stock: "AAPL", Field.Price: 100.53 } ] )
+    messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   20, Field.OrderRefNum: 2, Field.Side: 'B', Field.Shares: 300, Field.Stock: "AAPL", Field.Price: 100.55 } ] )
+    messages.append( [           MessageType.AddOrder, { Field.NanoSeconds:   30, Field.OrderRefNum: 3, Field.Side: 'B', Field.Shares: 400, Field.Stock: "AAPL", Field.Price: 100.56 } ] )
+    messages.append( [      MessageType.OrderExecuted, { Field.NanoSeconds:   40, Field.OrderRefNum: 2, Field.Shares: 300, Field.MatchNum: 1001 } ] )
+
 #messages.append( [ MessageType.OrderExecutedPrice, { Field.NanoSeconds:   1, Field.OrderRefNum: 1, Field.Shares: 200, Field.MatchNum: 101, Field.Printable: 'Y', Field.Price: 100.52 } ] )
 #messages.append( [        MessageType.OrderCancel, { Field.NanoSeconds:   1, Field.OrderRefNum: 1, Field.Shares: 200 } ] )
 #messages.append( [        MessageType.OrderDelete, { Field.NanoSeconds:   1, Field.OrderRefNum: 1} ] )
 #messages.append( [       MessageType.OrderReplace, { Field.NanoSeconds:   1, Field.OrderRefNum: 1, Field.NewOrderRefNum: 9, Field.Shares: 200, Field.Price: 100.52 } ] )
 
-genMessages = [ ]
-for message in messages:
-    msg = ItchMessageFactory.createFromArgs(message)
-    if msg:
-        genMessages.append(msg)
-
-outFile = "Itch.dat"
-print("-- # of generated messages: {0} --".format(len(genMessages)))
-i = 0
-for message in genMessages:
-    print("-- Dumping message #{0} --".format(i))
-    message.dumpPretty()
-    message.appendToFile(outFile)
-    #message.dumpRawBytes()
-    i += 1
+create_Test_1()
 
--- MarketData/MarketData_02/parseItch.py
+++ MarketData/MarketData_02/parseItch.py
@@ -1,11 +1,13 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
+#/usr/bin/env python3
 
 import struct
 from Itch41 import *
 
 #### Parameters for Execution
 # Download from here: ftp://emi.nasdaq.com/ITCH/11092013.NASDAQ_ITCH41.gz
-fileName = "/Users/john/Downloads/11092013.NASDAQ_ITCH41"
+#fileName = "/Users/john/Downloads/11092013.NASDAQ_ITCH41"
+fileName = "11092013.NASDAQ_ITCH41"
 outputFile = "Itch.dat"
 saveMessageTypes = [ 'A' ]
 numberOfMessagesToSave = 2
