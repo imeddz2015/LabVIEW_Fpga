697543bf1e2dd21e25bb33e1186e154202f3794e
John Stratoudakis <john@wallstreetfpga.com>
2018-02-28 07:10:28 -0500

    Updated LabVIEW code - this works! Will respond properly to ARP requests.  ICMP has a bug - but it is host side, no client side

--- 07_10_Gigabit_CLIP/d_microblaze_wrapper_top.vhd
+++ 07_10_Gigabit_CLIP/d_microblaze_wrapper_top.vhd
@@ -15,22 +15,7 @@ use UNISIM.VCOMPONENTS.ALL;
 entity d_microblaze_wrapper_top is
     port (
         ACLK : out STD_LOGIC;
-        AXI_STR_RXD_1_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-        AXI_STR_RXD_1_tlast : in STD_LOGIC;
-        AXI_STR_RXD_1_tready : out STD_LOGIC;
-        AXI_STR_RXD_1_tvalid : in STD_LOGIC;
-        AXI_STR_RXD_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-        AXI_STR_RXD_tlast : in STD_LOGIC;
-        AXI_STR_RXD_tready : out STD_LOGIC;
-        AXI_STR_RXD_tvalid : in STD_LOGIC;
-        AXI_STR_TXD_1_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-        AXI_STR_TXD_1_tlast : out STD_LOGIC;
-        AXI_STR_TXD_1_tready : in STD_LOGIC;
-        AXI_STR_TXD_1_tvalid : out STD_LOGIC;
-        AXI_STR_TXD_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-        AXI_STR_TXD_tlast : out STD_LOGIC;
-        AXI_STR_TXD_tready : in STD_LOGIC;
-        AXI_STR_TXD_tvalid : out STD_LOGIC;
+
         In0 : in STD_LOGIC_VECTOR ( 0 to 0 );
         In1 : in STD_LOGIC_VECTOR ( 0 to 0 );
         clock_rtl : in STD_LOGIC;
@@ -38,8 +23,31 @@ entity d_microblaze_wrapper_top is
         gpio_rtl_1_tri_i : in STD_LOGIC_VECTOR ( 31 downto 0 );
         gpio_rtl_2_tri_o : out STD_LOGIC_VECTOR ( 31 downto 0 );
         gpio_rtl_tri_i : in STD_LOGIC_VECTOR ( 31 downto 0 );
-        reset_rtl : in STD_LOGIC
+        reset_rtl : in STD_LOGIC;
 
+        AXI_STR_TXD_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+        AXI_STR_TXD_tkeep : out STD_LOGIC_VECTOR ( 3 downto 0 );
+        AXI_STR_TXD_tlast : out STD_LOGIC;
+        AXI_STR_TXD_tready : in STD_LOGIC;
+        AXI_STR_TXD_tvalid : out STD_LOGIC;
+
+        AXI_STR_RXD_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+        AXI_STR_RXD_tkeep : in STD_LOGIC_VECTOR ( 3 downto 0 );
+        AXI_STR_RXD_tlast : in STD_LOGIC;
+        AXI_STR_RXD_tready : out STD_LOGIC;
+        AXI_STR_RXD_tvalid : in STD_LOGIC;
+
+        AXI_STR_TXD_1_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+        AXI_STR_TXD_1_tkeep : out STD_LOGIC_VECTOR ( 3 downto 0 );
+        AXI_STR_TXD_1_tlast : out STD_LOGIC;
+        AXI_STR_TXD_1_tready : in STD_LOGIC;
+        AXI_STR_TXD_1_tvalid : out STD_LOGIC;
+
+        AXI_STR_RXD_1_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+        AXI_STR_RXD_1_tkeep : in STD_LOGIC_VECTOR ( 3 downto 0 );
+        AXI_STR_RXD_1_tlast : in STD_LOGIC;
+        AXI_STR_RXD_1_tready : out STD_LOGIC;
+        AXI_STR_RXD_1_tvalid : in STD_LOGIC
     );
 end d_microblaze_wrapper_top;
 
@@ -47,22 +55,7 @@ architecture STRUCTURE of d_microblaze_wrapper_top is
     component d_microblaze_wrapper is
         port (
             ACLK : out STD_LOGIC;
-            AXI_STR_RXD_1_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-            AXI_STR_RXD_1_tlast : in STD_LOGIC;
-            AXI_STR_RXD_1_tready : out STD_LOGIC;
-            AXI_STR_RXD_1_tvalid : in STD_LOGIC;
-            AXI_STR_RXD_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
-            AXI_STR_RXD_tlast : in STD_LOGIC;
-            AXI_STR_RXD_tready : out STD_LOGIC;
-            AXI_STR_RXD_tvalid : in STD_LOGIC;
-            AXI_STR_TXD_1_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-            AXI_STR_TXD_1_tlast : out STD_LOGIC;
-            AXI_STR_TXD_1_tready : in STD_LOGIC;
-            AXI_STR_TXD_1_tvalid : out STD_LOGIC;
-            AXI_STR_TXD_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
-            AXI_STR_TXD_tlast : out STD_LOGIC;
-            AXI_STR_TXD_tready : in STD_LOGIC;
-            AXI_STR_TXD_tvalid : out STD_LOGIC;
+
             In0 : in STD_LOGIC_VECTOR ( 0 to 0 );
             In1 : in STD_LOGIC_VECTOR ( 0 to 0 );
             gpio_rtl_0_tri_o : out STD_LOGIC_VECTOR ( 31 downto 0 );
@@ -70,7 +63,31 @@ architecture STRUCTURE of d_microblaze_wrapper_top is
             gpio_rtl_2_tri_o : out STD_LOGIC_VECTOR ( 31 downto 0 );
             gpio_rtl_tri_i : in STD_LOGIC_VECTOR ( 31 downto 0 );
             clock_rtl : in STD_LOGIC;
-            reset_rtl : in STD_LOGIC
+            reset_rtl : in STD_LOGIC;
+
+            AXI_STR_TXD_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+            AXI_STR_TXD_tkeep : out STD_LOGIC_VECTOR ( 3 downto 0 );
+            AXI_STR_TXD_tlast : out STD_LOGIC;
+            AXI_STR_TXD_tready : in STD_LOGIC;
+            AXI_STR_TXD_tvalid : out STD_LOGIC;
+
+            AXI_STR_RXD_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+            AXI_STR_RXD_tkeep : in STD_LOGIC_VECTOR ( 3 downto 0 );
+            AXI_STR_RXD_tlast : in STD_LOGIC;
+            AXI_STR_RXD_tready : out STD_LOGIC;
+            AXI_STR_RXD_tvalid : in STD_LOGIC;
+
+            AXI_STR_TXD_1_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+            AXI_STR_TXD_1_tkeep : out STD_LOGIC_VECTOR ( 3 downto 0 );
+            AXI_STR_TXD_1_tlast : out STD_LOGIC;
+            AXI_STR_TXD_1_tready : in STD_LOGIC;
+            AXI_STR_TXD_1_tvalid : out STD_LOGIC;
+
+            AXI_STR_RXD_1_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+            AXI_STR_RXD_1_tkeep : in STD_LOGIC_VECTOR ( 3 downto 0 );
+            AXI_STR_RXD_1_tlast : in STD_LOGIC;
+            AXI_STR_RXD_1_tready : out STD_LOGIC;
+            AXI_STR_RXD_1_tvalid : in STD_LOGIC
         );
     end component d_microblaze_wrapper;
 begin
@@ -78,22 +95,7 @@ begin
     d_microblaze_i: component d_microblaze_wrapper
     port map (
         ACLK => ACLK,
-        AXI_STR_RXD_1_tdata => AXI_STR_RXD_1_tdata,
-        AXI_STR_RXD_1_tlast => AXI_STR_RXD_1_tlast,
-        AXI_STR_RXD_1_tready => AXI_STR_RXD_1_tready,
-        AXI_STR_RXD_1_tvalid => AXI_STR_RXD_1_tvalid,
-        AXI_STR_RXD_tdata => AXI_STR_RXD_tdata,
-        AXI_STR_RXD_tlast =>AXI_STR_RXD_tlast,
-        AXI_STR_RXD_tready => AXI_STR_RXD_tready,
-        AXI_STR_RXD_tvalid => AXI_STR_RXD_tvalid,
-        AXI_STR_TXD_1_tdata => AXI_STR_TXD_1_tdata,
-        AXI_STR_TXD_1_tlast => AXI_STR_TXD_1_tlast,
-        AXI_STR_TXD_1_tready => AXI_STR_TXD_1_tready,
-        AXI_STR_TXD_1_tvalid => AXI_STR_TXD_1_tvalid,
-        AXI_STR_TXD_tdata => AXI_STR_TXD_tdata,
-        AXI_STR_TXD_tlast => AXI_STR_TXD_tlast,
-        AXI_STR_TXD_tready => AXI_STR_TXD_tready,
-        AXI_STR_TXD_tvalid => AXI_STR_TXD_tvalid,
+
         In0 => In0,
         In1 => In1,
         gpio_rtl_tri_i => gpio_rtl_tri_i,
@@ -101,6 +103,30 @@ begin
         gpio_rtl_1_tri_i => gpio_rtl_1_tri_i,
         gpio_rtl_2_tri_o => gpio_rtl_2_tri_o,
         clock_rtl => clock_rtl,
-        reset_rtl => reset_rtl
+        reset_rtl => reset_rtl,
+
+        AXI_STR_TXD_tdata => AXI_STR_TXD_tdata,
+        AXI_STR_TXD_tkeep => AXI_STR_TXD_tkeep,
+        AXI_STR_TXD_tlast => AXI_STR_TXD_tlast,
+        AXI_STR_TXD_tready => AXI_STR_TXD_tready,
+        AXI_STR_TXD_tvalid => AXI_STR_TXD_tvalid,
+
+        AXI_STR_RXD_tdata => AXI_STR_RXD_tdata,
+        AXI_STR_RXD_tkeep => AXI_STR_RXD_tkeep,
+        AXI_STR_RXD_tlast => AXI_STR_RXD_tlast,
+        AXI_STR_RXD_tready => AXI_STR_RXD_tready,
+        AXI_STR_RXD_tvalid => AXI_STR_RXD_tvalid,
+
+        AXI_STR_TXD_1_tdata => AXI_STR_TXD_1_tdata,
+        AXI_STR_TXD_1_tkeep => AXI_STR_TXD_1_tkeep,
+        AXI_STR_TXD_1_tlast => AXI_STR_TXD_1_tlast,
+        AXI_STR_TXD_1_tready => AXI_STR_TXD_1_tready,
+        AXI_STR_TXD_1_tvalid => AXI_STR_TXD_1_tvalid,
+
+        AXI_STR_RXD_1_tdata => AXI_STR_RXD_1_tdata,
+        AXI_STR_RXD_1_tkeep => AXI_STR_RXD_1_tkeep,
+        AXI_STR_RXD_1_tlast => AXI_STR_RXD_1_tlast,
+        AXI_STR_RXD_1_tready => AXI_STR_RXD_1_tready,
+        AXI_STR_RXD_1_tvalid => AXI_STR_RXD_1_tvalid
     );
 end STRUCTURE;
